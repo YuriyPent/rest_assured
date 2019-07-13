c99ec6b468dd413b7573157a2d1a1401ddf3b943
Yuriy <Seaman_on_watch@mail.ru>
2019-07-13 12:48:44 +0300

    Validating the body of first Json response

--- src/sed/instrument/ed/basics.java
+++ src/sed/instrument/ed/basics.java
@@ -4,10 +4,11 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class basics {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         RestAssured.baseURI = "https://maps.googleapis.com";
         given()
                 .param("location", "-33.8670522,151.1957362")
@@ -23,6 +24,8 @@ public class basics {
                 .then()
                 .assertThat().statusCode(200)
                 .and()
-                .contentType(ContentType.JSON);
+                .contentType(ContentType.JSON)
+                .and()
+                .body("results[0].name", equalTo("Harbour Bar & Kitchen"));
     }
 }

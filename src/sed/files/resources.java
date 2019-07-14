e525805c07c25eb72b988dbdab77527d8402ff15
Yuriy <Seaman_on_watch@mail.ru>
2019-07-14 19:13:22 +0300

    Datadriven testing best practises

---
+++ src/sed/files/resources.java
@@ -0,0 +1,8 @@
+package files;
+
+public class resources {
+
+    public static String placePostData() {
+        return "maps/api/place/add/json";
+    }
+}
--- src/sed/instrument/ed/basic3.java
+++ src/sed/instrument/ed/basic3.java
@@ -11,6 +11,8 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+import static files.payLoad.getPostData;
+import static files.resources.placePostData;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
@@ -28,27 +30,15 @@ public class basic3 {
     @Test
     public void AddAndDeletePlace() {
 
-        String body = "{\n" +
-                "    \"location\":{\n" +
-                "        \"lat\" : -38.383494,\n" +
-                "        \"lng\" : 33.427362\n" +
-                "    },\n" +
-                "    \"accuracy\":50,\n" +
-                "    \"name\":\"Frontline house\",\n" +
-                "    \"phone_number\":\"(+91) 983 893 3937\",\n" +
-                "    \"address\" : \"29, side layout, cohen 09\",\n" +
-                "    \"types\": [\"shoe park\",\"shop\"],\n" +
-                "    \"website\" : \"http://google.com\",\n" +
-                "    \"language\" : \"French-IN\"\n" +
-                "}\n";
+
 
 //        Add place
         RestAssured.baseURI = properties.getProperty("HOST");
         Response response = given()
-                .queryParam("key", "qaclick123")
-                .body(body)
+                .queryParam("key", properties.getProperty("KEY"))
+                .body(getPostData())
                 .when()
-                .post("maps/api/place/add/json")
+                .post(placePostData())
                 .then()
                 .assertThat()
                 .statusCode(200)
@@ -68,7 +58,7 @@ public class basic3 {
 
 //        Place this place_id in the Delete request
         given()
-                .queryParam("key", "qaclick123")
+                .queryParam("key", properties.getProperty("KEY"))
                 .body("{\"place_id\": \"" + placeId + "\"}")
                 .when()
                 .post("maps/api/place/delete/json")

58e61eac98a67f6c028ba1c52a7016e8225f5838
Yuriy <Seaman_on_watch@mail.ru>
2019-07-16 22:19:46 +0300

    Understanding TestNg Data provider for parameterization

--- src/test/tests/DynamicJson.java
+++ src/test/tests/DynamicJson.java
@@ -5,6 +5,7 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -14,8 +15,9 @@ import static io.restassured.RestAssured.given;
 
 public class DynamicJson {
 
-    @Test
+    @Test(dataProvider = "BooksData")
     public void addBook() throws IOException {
+
         RestAssured.baseURI = "http://216.10.245.166";
         Response response = given()
                 .header("Content-Type", "application/json")
@@ -30,5 +32,19 @@ public class DynamicJson {
 
         JsonPath jsonPath = ReusableMethods.rawToJSON(response);
         System.out.println("ID: \n" + jsonPath.get("ID"));
+    }
+
+    @DataProvider(name = "BooksData")
+    public Object[][] getData() {
+
+        return new Object[][] {
+            {
+                "iop", "9873"
+            },{
+                "frf", "8984"
+            },{
+                "iuyp", "9033"
+            }
+        };
     }
 }

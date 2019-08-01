207dc30e70de6957b73f4dc6f1d8cf2876248c72
Yuriy <Seaman_on_watch@mail.ru>
2019-08-01 17:59:39 +0300

    Getting rows and its cells from Sheet

--- ExcelDriven/src/test/java/dataDriven.java
+++ ExcelDriven/src/test/java/dataDriven.java
@@ -1,19 +1,41 @@
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Iterator;
 
 public class dataDriven {
 
+//    once coloumn is identifired then scan entire testcase coloumn to identify purchase testcase raw
+//    after grab purchase testcase row=pull all the data of that row and feed into test
+
     public static void main(String[] args) throws IOException {
 
+//        File input stream argument
         FileInputStream fileInputStream = new FileInputStream("ExcelDriven/src/main/resources/datademo.xlsx");
         XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream);
+
         int sheets = workbook.getNumberOfSheets();
+
         for (int i = 0; i < sheets; i++) {
+
             if (workbook.getSheetName(i).equalsIgnoreCase("testdata")) {
                 XSSFSheet xssfSheet = workbook.getSheetAt(i);
+//    Identify testcases coloumn by scanning the entire 1st row
+
+                Iterator<Row> rows = xssfSheet.iterator(); //sheet is collection of rows
+                Row firstrow = rows.next();
+                Iterator<Cell> cell = firstrow.cellIterator(); //row is collections of cells
+
+                while (cell.hasNext()) {
+                    Cell value = cell.next();
+                    if (value.getStringCellValue().equalsIgnoreCase("TestCases")) {
+//                        desired coloumn
+                    }
+                }
             }
         }
     }
